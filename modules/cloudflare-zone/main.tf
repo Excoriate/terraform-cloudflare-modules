7389dbe2a1558fb90b0b93ecbe75735258f36122
Alex ATorres <atorres.ruiz@hotmail.com>
2024-03-10 12:24:54 +0100

    fix: adjust each.value for indexing strings

--- modules/cloudflare-zone/main.tf
+++ modules/cloudflare-zone/main.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_zone" "this" {
   for_each   = local.domains_to_create
   account_id = var.cloudflare_account_id
-  zone       = each.value["domain"]
+  zone       = each.value
 }
