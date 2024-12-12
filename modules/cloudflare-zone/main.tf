5f0d68455a59f39f6d7add0b7b6497a1cec5360e
Alex ATorres <atorres.ruiz@hotmail.com>
2024-12-12 07:42:12 +0100

    feat: Add account ID to Cloudflare zone resource
    Add the account ID to the Cloudflare zone resource to ensure proper configuration.

--- modules/cloudflare-zone/main.tf
+++ modules/cloudflare-zone/main.tf
@@ -1,5 +1,7 @@
 resource "cloudflare_zone" "this" {
   for_each   = local.domains_to_create
-  account = var.cloudflare_account_id
+  account = {
+    id = var.cloudflare_account_id
+  }
   name       = each.value
 }
